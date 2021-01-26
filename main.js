6d715a5e111f525729f52841d39f6e1cf720b968
Louise <louisepeiffer@hotmail.com>
2021-01-26 09:41:50 +0100

    exos finis

---
+++ main.js
@@ -0,0 +1,26 @@
+// 1
+let mesTitres = document.getElementsByTagName('h1')
+console.log(mesTitres);
+
+// 2
+let lesh3 = document.getElementsByTagName('h3')
+console.log(lesh3);
+
+let firstH3 = document.getElementsByTagName('h3')[0]
+let secondH3 = document.getElementsByTagName('h3')[1]
+let thirdH3 = document.getElementsByTagName('h3')[2]
+console.log(firstH3);
+console.log(secondH3);
+console.log(thirdH3);
+
+// 3
+let lesPar = document.getElementsByTagName('p')
+console.log(lesPar);
+let secondPar = document.getElementsByTagName('p')[1]
+console.log(secondPar);
+
+// 4
+let lesLi = document.getElementsByTagName('li')
+console.log(lesLi);
+let thirdLi = document.getElementsByTagName('li')[2]
+console.log(thirdLi);
